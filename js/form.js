fde618eeb5a9229b92e34c49d15be2c76d848b38
Kay Wolter <kay.wolter@codeva.de>
2015-04-01 15:14:06 +0200

    added contact form templates

---
+++ js/form.js
@@ -0,0 +1,45 @@
+var form = angular.module( "form", [] );
+
+form.controller( "FormCtrl", function ( $scope, $http ){
+  this.isFormSent = false;
+  this.isError = false;
+  this.fields = {};
+
+  this.sendForm = function(){
+    var thisCtrl = this;
+    requestData = {
+      senderEmail: this.fields.email,
+      senderMessage: this.fields.text
+    }
+    if ( this.fields.name ){
+      requestData.senderName = this.fields.name;
+    }
+    if ( this.fields.phone ){
+      requestData.senderPhone = this.fields.phone;
+    }
+    
+    $http.post('php/formMailer.php', requestData).
+    success( function( data, status, headers, config ){
+      console.log( "STATUS:", status );
+      thisCtrl.onSuccess();
+    }).
+    error( function( data, status, headers, config ){
+      console.log( "STATUS:", status );
+      thisCtrl.onFailure();
+    });
+  }
+
+  this.onSuccess = function(){
+    this.isFormSent = true;
+    this.isError = false;
+    this.fields = {};
+    $scope.contactForm.$setPristine();
+    $scope.contactForm.$setSubmitted();
+  }
+  
+  this.onFailure = function(){
+    this.isFormSent = false;
+    this.isError = true;
+  }
+  
+});
